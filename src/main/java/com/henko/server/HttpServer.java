60d31cf3442d61faf07b11cb5b6a69f47462e221
Ruslan Kurchenko <henko.okdev@gmail.com>
2015-03-04 20:56:06 +0200

    implemented basic server logic

---
+++ src/main/java/com/henko/server/HttpServer.java
@@ -0,0 +1,53 @@
+package com.henko.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+
+public class HttpServer {
+    final static boolean SSL = System.getProperty("ssl") != null;
+    final static int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
+
+    public static void main(String[] args) throws Exception {
+        // Configure SSL
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+
+            // .certificate() - certificate file| .privateKey() - private key in RSA format
+            sslCtx = SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
+        } else {
+            sslCtx = null;
+        }
+
+        // Configure the server
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap()
+                    .option(ChannelOption.SO_BACKLOG, 1024) //TODO: what does SO_BLACK?
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpServerInitializer(sslCtx));
+            
+            Channel ch = b.bind(PORT).sync().channel();
+            
+            System.err.println("HTTP server started... \n" +
+                    "navigate to " + (SSL? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+            
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/henko/server/HttpServerInitializer.java
@@ -0,0 +1,25 @@
+package com.henko.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+
+
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel>{
+    
+    private final SslContext sslCtx;
+
+    public HttpServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+        if (sslCtx != null) p.addLast(sslCtx.newHandler(ch.alloc()));
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpServerHandler());
+    }
+}
